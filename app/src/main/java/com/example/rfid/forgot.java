b2c43c2540e38bb399437beec45ceda33952e911
chrollo1864 <cpe.bisco.charles@gmail.com>
2025-09-22 11:02:17 +0800

    fixed and updates

--- app/src/main/java/com/example/rfid/forgot.java
+++ app/src/main/java/com/example/rfid/forgot.java
@@ -1,10 +1,6 @@
 package com.example.rfid;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,31 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+public class forgot extends AppCompatActivity {
 
-
-public class MainActivity extends AppCompatActivity {
-    private TextView forgot;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_forgot);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
         });
-
-
-        forgot=findViewById(R.id.forgot);
-        forgot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this, forgot.class);
-                startActivity(intent);
-            }
-        });
-
     }
 }
